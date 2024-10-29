c6a30ba174548e89fb8e43f5a891b2a2ea375689
Karen <ipepela1@mail.ru~>
2024-10-29 22:44:45 +0300

    история

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿static List<string> LoadHistory()
+{
+    if (File.Exists("history.txt"))
+    {
+        return new List<string>(File.ReadAllLines("history.txt"));
+    }
+    return new List<string>();
+}
